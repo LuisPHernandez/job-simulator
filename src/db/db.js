e5a9490685d4e7a9b583814d7adac9654e683637
LuisPHernandez <lphm0105@gmail.com>
2026-03-19 21:14:40 -0600

    CORS

--- src/db/db.js
+++ src/db/db.js
@@ -2,9 +2,9 @@ const { Pool } = require('pg')
 
 const pool = new Pool({
     host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
     port: process.env.DB_PORT,
 })
 
