75d65d1328c5b41b5a4aa5ceb4fb088c9cc96799
LuisPHernandez <lphm0105@gmail.com>
2026-03-19 20:36:48 -0600

    init.sql

---
+++ init.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS products (
+  id       SERIAL PRIMARY KEY,
+  name     VARCHAR(255) NOT NULL,
+  brand    VARCHAR(255) NOT NULL,
+  category VARCHAR(255) NOT NULL,
+  stock    INTEGER NOT NULL,
+  price    NUMERIC(10,2) NOT NULL,
+  available BOOLEAN NOT NULL
+);
